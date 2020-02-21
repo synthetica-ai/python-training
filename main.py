f49ea80777d9aad867678b01b6bfb921b1f2df67
stefanosKarantin <karantinakis.s@gmail.com>
2020-02-21 13:41:29 +0200

    creating new version

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#htht
+from itertools import chain, product
+from random import shuffle, sample
+
+suits = ['diamonds', 'hearts', 'spades', 'clubs']
+
+rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+def createCard(combination):
+    card = {}
+    card['rank'], card['suit'] = combination
+
+    return card
+
+def createDeck():
+    return list(map(createCard, chain(product(rank, suits))))
+
+def shuffleDeck(deck):
+    shuffle(deck)
+
+    return deck
+
+def getHand(deck):
+    hand = sample(deck, 5)
+
+    updatedDeck = list(map(deck.remove, hand))
+    return  hand, updatedDeck
+
+deck = createDeck()
+
+shuffleDeck(deck)
+
+player1, deck = getHand(deck)
+player2, deck = getHand(deck)
+
