74b845ee41e16ec7a30b6ef04aa37cb71b3aba3a
Pagonis-Dimitris <pagonisdc@gmail.com>
2020-03-10 16:28:26 +0200

    newpoke

---
+++ poker/Deck.py
@@ -0,0 +1,67 @@
+from collections import Counter
+from functools import reduce
+from itertools import chain, product
+from random import sample, shuffle
+
+from card import Card
+
+class Deck:
+    rank=[i for i in range(2,15)]
+    suit=["Spades", "Hearts", "Clubs", "Diamonds"]
+
+    def __init__(self):
+        self.cards=list(map(Deck.create, chain(product(Deck.rank, Deck.suit))))
+
+    def create(args):
+        c=Card(*args)
+        return c.__str__()
+
+    def deck_shuffle(self):
+        shuffle(self.cards)
+
+
+    def take_hand(self):
+        h=list(sample(self.cards,5))
+        for k in h:
+            self.cards.remove(k)
+        return h
+
+def suits(k):
+    def func(i):
+        for k in i.values():
+            return k
+    new_l=list(map(func, k))
+    flag=reduce(lambda x,y: x if x==y else 0 ,new_l)
+    return bool(flag)
+
+def sort_hand(k):
+    def func(i):
+        for k in i.keys():
+            return k
+    new_l=list(map(func, k))
+    return new_l
+
+def evaluate(s,r):
+    c=Counter(r)
+    max_times=max(c.values())
+    high=[k for k,l in c.items() if l==max_times]
+    high.sort(reverse=True)
+    flush=reduce(lambda a,b: b if (a-b==1) else 0, r)
+    four=True if max_times==4 else False
+    full=True if (max_times==3 and len(c)==2)  else False
+    straight=s
+    three=True if max(c.values())==3 else False
+    two_pairs=True if max_times==2 and len(c)==3 else False
+    pair=True if max_times==2 else False
+    eval=[straight and flush,
+        four,
+        full,
+        flush,
+        straight,
+        three,
+        two_pairs,
+        pair,
+        high]
+
+    evalindex = [bool(value) for value in eval].index(True)
+    return evalindex, high
---
+++ poker/poker.py
@@ -0,0 +1,34 @@
+from Deck import *
+
+
+deck1=Deck()
+
+deck1.deck_shuffle()
+
+hand1=deck1.take_hand()
+hand2=deck1.take_hand()
+
+print(hand1)
+print(hand2)
+
+suit_h1=suits(hand1)
+suit_h2=suits(hand2)
+
+s_h1=sort_hand(hand1)
+s_h2=sort_hand(hand2)
+
+ev1,high1=evaluate(suit_h1, s_h1)
+ev2,high2=evaluate(suit_h2, s_h2)
+
+if ev1<ev2:
+    winner=1
+elif ev2<ev1:
+    winner= 2
+elif high1>high2:
+    winner=1
+elif high2>high1:
+    winner= 2
+else:
+    print("draw")
+    exit()
+print(f"Player {winner} wins")
